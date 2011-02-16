922ad63ed1617d9027e7c21a57c5bed8226bee30
zb <zb@3e8.org>
2011-02-15 23:12:54 -0600

    feature-test: add DP, RP

--- feature-test.scm
+++ feature-test.scm
@@ -1,21 +1,37 @@
 (define-syntax R
- (syntax-rules ()
-  ((_ F) (begin (define-foreign-variable F bool)
-                ((if F R! U!) 'F)))))
+  (lambda (e r c)
+    (let ((F (->string (cadr e))))
+      (let ((%begin (r 'begin))
+            (%define-foreign-variable (r 'define-foreign-variable))
+            (%bool (r 'bool)) (%quote (r 'quote))
+            (%if (r 'if)) (%R! (r 'R!)) (%U! (r 'U!)))
+        (let* ((cvar (string-append *ft:definition-prefix* F))
+               (var (string->symbol cvar))
+               (ft (string->symbol (string-append *ft:registration-prefix* F))))
+          `(,%begin (,%define-foreign-variable ,var ,%bool ,cvar)
+                    ((,%if ,var ,%R! ,%U!)
+                     (,%quote ,ft))))))))
 (define-syntax D
   (er-macro-transformer
    (lambda (e r c)
-     (let ((d (->string (cadr e))))
+     (let ((d (->string (cadr e)))
+           (dp *ft:definition-prefix*))
        `(,(r 'foreign-declare)
-         ,(sprintf "#ifdef ~A\n#define HAVE_~A 1\n#else \n#define HAVE_~A 0\n#endif\n"
-                   d d d))))))
+         ,(sprintf "#ifdef ~A\n#define ~A~A 1\n#else \n#define ~A~A 0\n#endif\n"
+                   d dp d dp d))))))
 (define-syntax DR
+  (syntax-rules () ((DR F) (begin (D F) (R F)))))
+(define-syntax DP
   (lambda (e r c)
-    (let ((d (->string (cadr e))))
-      `(,(r 'begin)
-        (,(r 'D) ,d)
-        (,(r 'R) ,(string->symbol (string-append "HAVE_" d)))))))
+    (set! *ft:definition-prefix* (->string (cadr e)))
+    `(,(r 'begin))))
+(define-syntax RP
+  (lambda (e r c)
+    (set! *ft:registration-prefix* (->string (cadr e)))
+    `(,(r 'begin))))
+
+(define *definition-prefix* "HAVE_")
+(define *registration-prefix* "")
 
 (define R! (lambda (f) (printf "(register-feature! '~S)\n" f)))
 (define U! (lambda (f) (printf "(unregister-feature! '~S)\n" f)))
-
