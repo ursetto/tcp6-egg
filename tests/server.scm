8310150fcc1beb9d1b3288f8df84cf54248bbb01
Jim Ursetto <jim@3e8.org>
2018-11-13 22:44:25 -0600

    server prints local and remote address/ports

--- tests/server.scm
+++ tests/server.scm
@@ -10,6 +10,11 @@
 (define l (tcp-listen 4242))
 (let loop ()
     (define-values (i o) (tcp-accept l))
+    (let-values (((local-addr remote-addr) (tcp-addresses i))
+                 ((local-port remote-port) (tcp-port-numbers i)))
+      (printf "** Connect from [~A]:~A to [~A]:~A\n" 
+              remote-addr remote-port
+              local-addr local-port))
     (write-line (sprintf "Server says hello at ~A!" (seconds->string)) 
                 o)
     (print (read-line i))
